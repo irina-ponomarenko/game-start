2537b60d23e939d5d8ca6cfecf1ca0087c560af3
Ирина <ponomarenko02041993@gmail.com>
2020-02-10 22:25:55 +0200

    start

---
+++ js/main.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+
+    //---------------slider--------------//
+
+    let handler = function (e,ui) {
+        let value = (jQuery(this).offset().left - $(".draggable-container").offset().left),
+            prc   = value / ($(".draggable-container")[0].offsetWidth / 100);
+        
+        if(prc < 50) {
+            prc = Math.floor(prc/10);
+        }
+    };
+
+    jQuery(function(){
+        $( "#draggable" ).draggable({
+            axis: "x",
+            containment: "parent",
+            drag: handler
+        });
+    });
+
+
+    //-------------counter----------//
+
+    $('.minus').click(function () {
+        let $input = $(this).closest('.number-counter').find('.counter-input');
+        let count = parseInt($input.val()) - 1;
+        count = count < 1 ? 1 : count;
+        $input.val(count);
+        $input.change();
+        return false;
+    });
+    $('.plus').click(function () {
+       let $input = $(this).closest('.number-counter').find('.counter-input');
+        $input.val(parseInt($input.val()) + 1);
+        $input.change();
+        return false;
+    });
+
+});
