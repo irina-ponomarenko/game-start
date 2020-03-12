2e9ea2fce3d2ecdc0d5b9b94d356b071beb77e66
Ирина <ponomarenko02041993@gmail.com>
2020-03-12 15:23:13 +0200

    телефон и планшет

--- js/main.js
+++ js/main.js
@@ -387,26 +387,5 @@ $(document).ready(function() {
         });
     }, 12000);
 
-    $(window).resize(function() {
-        if ( $(window).width() < 820 ) {
-            window.onload = function () {
-                hideAddressBar();
-                window.addEventListener("orientationchange", function () {
-                    hideAddressBar();
-                }, false);
-            };
-
-            function hideAddressBar() {
-                setTimeout(function () {
-                    document.body.style.height = window.outerHeight + 'px';
-                    setTimeout(function () {
-                        window.scrollTo(0, 1);
-                    }, 1100);
-                }, 1000);
-                return false;
-            };
-        };
-    });
-
 
 });
